c74ab09c5455be904634bf6d52d1f3b3cadf4c77
volodin7ergey <volodin7ergey@gmail.com>
2026-04-06 18:11:18 +0300

    added report

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import Report from './Report.vue';
+</script>
+
+<template>
+<div class="base-layout">
+  <Report />
+</div>
+</template>
+
+<style scoped>
+.base-layout {
+  padding: 1rem;
+}
+</style>
+
+
+
---
+++ src/bitrix.d.ts
@@ -0,0 +1,1 @@
+declare const BX24: any
